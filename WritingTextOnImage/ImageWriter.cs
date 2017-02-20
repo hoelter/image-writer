e31777167f73132c2eee58dc4e1a4fc549bf4baf
hoelter <hoelterc@gmail.com>
2017-02-19 22:48:43 -0600

    Quick test of writing text to an image using an external font.

---
+++ WritingTextOnImage/ImageWriter.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.Runtime.Remoting.Messaging;
+
+namespace WritingTextOnImage
+{
+    public class ImageWriter
+    {
+        public void WriteTextToImage(Bitmap image, string savePath, string textToWrite, FontFamily fontFamily)
+        {
+            using (var graphics = Graphics.FromImage(image))
+            {
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                // Image is 600x600 pixels, this places it upper-center
+                PointF writeLocation = new PointF(222f, 124f);
+                var format = new StringFormat { Alignment = StringAlignment.Center };
+
+                using (var font = new Font(fontFamily, 15, FontStyle.Regular))
+                {
+                    graphics.DrawString(textToWrite, font, Brushes.White, writeLocation, format);
+                }
+                image.Save(savePath, ImageFormat.Jpeg);
+            }
+        }
+
+        public Bitmap LoadImage(string imagePath)
+        {
+            Bitmap image = new Bitmap(imagePath);
+            return image;
+        }
+
+        public FontFamily LoadFont(string fontPath, string fontName)
+        {
+            PrivateFontCollection fonts = new PrivateFontCollection();
+            fonts.AddFontFile(fontPath);
+            FontFamily font = fonts.Families[0];
+            return font;
+        }
+    }
+}
--- WritingTextOnImage/Program.cs
+++ WritingTextOnImage/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,18 @@ namespace WritingTextOnImage
     {
         static void Main(string[] args)
         {
+            string rootProjectPath = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string imagePath = Path.Combine(rootProjectPath + "\\prometheus.jpg");
+            string fontPath = Path.Combine(rootProjectPath + "\\Chunkfive.otf");
+            string savePath = Path.Combine(rootProjectPath + "\\new_prometheus.jpg");
+
+            var imageWriter = new ImageWriter();
+            Bitmap image = imageWriter.LoadImage(imagePath);
+            FontFamily font = imageWriter.LoadFont(fontPath, "chunkfive");
+
+            imageWriter.WriteTextToImage(image, savePath, "Prometheus", font);
+
+            //Console.ReadLine();
         }
     }
 }
